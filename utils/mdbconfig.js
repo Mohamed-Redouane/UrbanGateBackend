8ebf05d7ec2e322b1965eb00a39f39d269ab081d
Mohamed-Redouane <126030228+Mohamed-Redouane@users.noreply.github.com>
2023-11-18 23:21:39 -0500

    up

--- utils/mdbconfig.js
+++ utils/mdbconfig.js
@@ -1,13 +1,14 @@
  //Reference : https://blog.appsignal.com/2023/08/09/how-to-use-mongodb-and-mongoose-for-nodejs.html
  import mongoose from "mongoose";
+ import dotenv from 'dotenv';
 
-
+ dotenv.config();
 let db = null;
 
 async function connectDB() {
   if (!db) {
         try {
-            const connection = await mongoose.connect(`mongodb+srv://Cluster96567:SOEN341@cluster96567.wpnpdsc.mongodb.net/soen341test`);
+            const connection = await mongoose.connect(process.env.MONGODB_URI);
             db = connection;
         } catch (err) {
             console.error('Failed to connect to MongoDB', err);
